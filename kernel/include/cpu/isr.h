1b96d2c4561e88491bf434587d33f96b9087e01d
m-deh <badil2013@hotmail.com>
2018-07-13 21:13:50 +0430

    ISRs definiations

---
+++ kernel/include/cpu/isr.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2018 M D
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+#ifndef CPU_ISR_H
+#define CPU_ISR_H
+
+#include "type.h"
+
+void isr_install();
+void irq_install();
+
+#endif
