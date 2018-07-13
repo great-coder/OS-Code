9afc2e85305b99c336c2a2f62426e99a7a70d63a
m-deh <badil2013@hotmail.com>
2018-07-13 12:51:49 +0430

    Interrupt Descriptor Table(IDT)

---
+++ kernel/arch/x86/cpu/idt.c
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2018 M D
+ * 
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+#include "cpu/idt.h"
+#include "../type.h"
+
+#define KERNEL_CS 0x08
+#define IDT_ENTRIES 256
+
+idt_descriptor_t idt[IDT_ENTRIES];
+idt_register_t idt_reg;
+
+// Enabled?
+static inline u8 are_interrupts_enabled()
+{
+    unsigned long flags;
+    asm volatile("pushf\n\t"
+                 "pop %0"
+                 : "=g"(flags));
+    return flags & (1 << 9);
+}
+
+void set_idt_gate(u8 i, u32 callback)
+{
+    idt[i].low_offset = low_16(callback);
+    idt[i].selector = KERNEL_CS;
+    idt[i].zero = 0;
+    idt[i].type_attr = 0x8E;
+    idt[i].high_offset = high_16(callback);
+}
+
+// LIDT
+static inline void lidt()
+{
+    // This function works in 32 and 64bit mode
+    idt_reg.base = (u32)&idt; // For 64bit change this to u64
+    idt_reg.limit = IDT_ENTRIES * sizeof(idt_descriptor_t) - 1;
+    asm("lidt %0"
+        :
+        : "m"(idt_reg)); // let the compiler choose an addressing mode
+}
---
+++ kernel/include/cpu/idt.h
@@ -0,0 +1,26 @@
+// Copyright (c) 2018 M D
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+#ifndef CPU_INT_H
+#define CPU_INT_H
+
+#include "type.h"
+
+/*
+ * Returns a true boolean value if irq are enabled for the CPU.
+ */
+static inline u8 are_interrupts_enabled();
+
+/*
+ * Puts an interrupt in table
+ */
+void set_idt_gate(u8 i, u32 callback);
+
+/*
+ * Defines a new interrupt table.
+ */
+static inline void lidt();
+
+#endif
