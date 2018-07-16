dfbec27ac83bdb989909c0efe4bab79bee765f2a
m-deh <badil2013@hotmail.com>
2018-07-16 15:36:43 +0430

    Update x86 config file

--- kernel/include/config.h
+++ kernel/include/config.h
@@ -1,5 +1,5 @@
 // Copyright (c) 2018 M D
-// 
+//
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
@@ -7,19 +7,21 @@
 #define CONFIG_H
 
 #define KERNEL_NAME "eXtreme"
-#define KERNEL_VERSION "0.0.1"
-#define KERNEL_RELEASE_RING "ALPHA"
+#define KERNEL_VERSION "0.0.2"
+#define KERNEL_RELEASE_RING "alpha"
 #define KERNEL_DATE __DATE__
 #define KERNEL_TIME __TIME__
 #define KERNEL_LICENCE "MIT"
 #define KERNEL_MACHINENAME "root"
 #define OS_NAME "VOS"
 
-#ifdef __x86__
+// #ifndef __x86__
+// #define __x86__
+// #else
+// #define KERNEL_PROCESSOR_IDENTIFIER "NULL"
+// #endif
+
 #define KERNEL_PROCESSOR_IDENTIFIER "x86"
-#else
-#define KERNEL_PROCESSOR_IDENTIFIER "NULL"
-#endif
 
 #define CONFIG_MAX_FILE 32
 
